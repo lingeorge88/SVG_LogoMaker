e0443e91847fe2d7fbb16f3074ed19dce504b12a
lingeorge88 <lingeorge04@gmail.com>
2023-03-13 12:33:17 -0700

    Added file structure and starter codes for javascript and npm packages

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const shapes = requrie('./lib/shapes');
+
+const userQuestions =[
+    {
+        type: 'list',
+        name: 'logoShape',
+        message: `Please choose logo shape`,
+        choices: ['triangle', 'circle', 'square'],
+      },
+    {
+        type:"input",
+        name:"text",
+        message: "Please enter up to 3 characters for your logo text",
+    },
+    {
+        type:"input",
+        name:"textColor",
+        message: "Please enter a color keyword or hexadecimal number for your text's color",
+    },
+    {
+        type:"input",
+        name:"logoColor",
+        message: "Please enter a color keyword or hexadecimal number for your logo's color",
+    },
+] 
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+    inquirer.prompt(userQuestions).then((responses) => {
+        console.log("Generated logo.svg");
+        writeToFile("./examples/logo.svg", shapes({ ...responses }));
+      });
+}
+init();
